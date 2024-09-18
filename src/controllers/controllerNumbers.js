4c73f2a248285c5e47753bef268289c49a4d50ff
rhuan-rgb <rhuan.lima.cmd@gmail.com>
2024-09-18 09:43:37 -0300

    correção 18-09

---
+++ src/controllers/controllerNumbers.js
@@ -0,0 +1,23 @@
+module.exports = class controllerNumber {
+  static async checkPar(req, res) {
+    const { numero } = req.body;
+    if (numero % 2 === 0) {
+      return res.status(200).json({ message: "Número Par!" });
+    }
+    return res.status(200).json({ message: "Numero Ímpar!" });
+  }
+
+  static async checkPrimo(req, res) {
+    const {numero} = req.body;
+    if (numero <= 1) {
+      return res.status(200).json({ message: "Não é primo!" });
+    }
+
+    for (let i = 2; i < numero; i++) {
+      if (numero % i == 0) {
+        return res.status(200).json({message: "Não é primo!" });
+      }
+    }
+    return res.status(200).json({ message: "É primo!" });
+  }
+};
